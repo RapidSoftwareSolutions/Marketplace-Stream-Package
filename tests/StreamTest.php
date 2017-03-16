1e5d8b1df092d66f6c9b7062be76879474643899
ustal <quit.ipad2@gmail.com>
2017-03-16 18:45:28 +0200

    add tests

---
+++ tests/StreamTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Tests;
+
+require_once(__DIR__ . '/../src/Models/checkRequest.php');
+
+class GeoRankerTest extends BaseTestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testRoutes($url) {
+        $response = $this->runApp("POST", '/api/Stream/'.$url);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function dataProvider() {
+        return [
+            ['createActivity'],
+            ['getActivities'],
+            ['deleteActivity'],
+            ['followFeed'],
+            ['unFollowFeed'],
+            ['getFeedFollower'],
+            ['getFeedFollowings'],
+            ['createMultipleFollows'],
+            ['addActivityToFeeds']
+        ];
+    }
+}

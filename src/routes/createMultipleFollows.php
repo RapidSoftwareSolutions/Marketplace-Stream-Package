0219352286df10f7bb187ca6ef3f6d129f8fa8ac
ustal <quit.ipad2@gmail.com>
2017-05-29 10:25:03 +0300

    fix file and array

--- src/routes/createMultipleFollows.php
+++ src/routes/createMultipleFollows.php
@@ -12,20 +12,38 @@ $app->post('/api/Stream/createMultipleFollows', function ($request, $response) {
         $postData = $validateRes;
     }
 
-    try {
-        $client = new GetStream\Stream\Client($postData['args']['apiKey'], $postData['args']['apiSecret']);
-        $batcher = $client->batcher();
-        $responseFromVendor = $batcher->followMany($postData['args']['list']);
-        $result['callback'] = 'success';
-        $result['contextWrites']['to'] = $responseFromVendor;
-    } catch (\GuzzleHttp\Exception\BadResponseException $exception) {
+    if (is_array($postData['args']['list'])) {
+        $data = $postData['args']['list'];
+    }
+    else {
+        $toJson = new \Models\normilizeJson();
+        $data = $toJson->normalizeJson(file_get_contents($postData['args']['list']));
+        $data = str_replace('\"', '"', $data);
+        $data = json_decode($data, true);
+    }
+
+
+    if (!empty($data)) {
+        try {
+            $client = new GetStream\Stream\Client($postData['args']['apiKey'], $postData['args']['apiSecret']);
+            $batcher = $client->batcher();
+            $responseFromVendor = $batcher->followMany($postData['args']['list']);
+            $result['callback'] = 'success';
+            $result['contextWrites']['to'] = $responseFromVendor;
+        } catch (\GuzzleHttp\Exception\BadResponseException $exception) {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = json_decode($exception->getResponse()->getBody());
+        } catch (\GetStream\Stream\StreamFeedException $exception) {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = json_decode($exception->getMessage());
+        }
+    }
+    else {
         $result['callback'] = 'error';
         $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-        $result['contextWrites']['to']['status_msg'] = json_decode($exception->getResponse()->getBody());
-    } catch (\GetStream\Stream\StreamFeedException $exception) {
-        $result['callback'] = 'error';
-        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-        $result['contextWrites']['to']['status_msg'] = json_decode($exception->getMessage());
+        $result['contextWrites']['to']['status_msg'] = 'List cannot be empty';
     }
 
 
